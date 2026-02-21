04fda01233500d717e96043256177be782374222
Michael Chenetz <mchenetz@users.noreply.github.com>
2026-02-20 22:46:16 -0500

    runtime: keep planner and scheduler pods alive

--- cmd/planner/main.go
+++ cmd/planner/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	log.Println("splai planner starting")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+	log.Println("splai planner shutting down")
 }
--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	log.Println("splai scheduler starting")
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+	log.Println("splai scheduler shutting down")
 }
